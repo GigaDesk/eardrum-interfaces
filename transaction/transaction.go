3a1234308c01286995957158c78e3386ea5d59ce
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-06-14 17:58:10 +0300

    refactor: redefine Transaction and NewTransaction fields

--- transaction/transaction.go
+++ transaction/transaction.go
@@ -15,13 +15,12 @@ type Transaction interface {
 	GetUpdatedAt() time.Time // Returns the last update timestamp of the transaction
 	GetDeletedAt() time.Time // Returns the deletion timestamp if the transaction is currently deleted or 0001-01-01 00:00:00 +0000 UTC if it is not
 	GetTotalAmountInCents() int64   // Returns the total amount spent in the transaction in cents
-	GetBalanceBeforeInCents() int64 // Returns the account balance before the transaction in cents
-	GetBalanceAfterInCents() int64  // Returns the account balance after the transaction in cents.
+	GetTransactionCostInCents() int64 // Returns the transaction cost in cents
 }
 
 // NewTransaction represents data fed into the system with the aim of creating a new transaction entity.
 type NewTransaction interface {
 	GetPurchasedProducts() []PurchasedProduct //Returns information on all product purchases involved in the transaction
-	GetRegistrationNumber() //Returns the registration number provided by the student to authorize the transaction
-	GetPinCode() //Returns the pin code provided by the student to authorize the transaction
+	GetPhoneNumber() //Returns the phone number provided by the user to authorize the transaction
+	GetPinCode() //Returns the pin code provided by the user to authorize the transaction
 }
