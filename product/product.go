c61db0b27168a05a5ba58ed891b2e63fcbf83c88
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-08-13 14:59:15 +0300

    refactor: add GetDeleted, GetCategoryID methods  to product interface and GetBlocked method to product and category interfaces

--- product/product.go
+++ product/product.go
@@ -6,11 +6,14 @@ import "time"
 type Product interface {
 	GetID() int64                  // Returns the unique identifier of the product
 	GetShopID() int64              // Returns the unique identifier of the shop that the product belongs to
+	GetCategoryID() int64          // Returns the unique identifier of the category that the product belongs to
 	GetCreatedAt() time.Time       // Returns the creation timestamp of the product
 	GetUpdatedAt() time.Time       // Returns the last update timestamp of the product
 	GetDeletedAt() time.Time       // Returns the deletion timestamp if the product is currently deleted or 0001-01-01 00:00:00 +0000 UTC if it is not
 	GetName() string               // Returns the name of the product
 	GetPricePerUnitInCents() int64 // Returns the product's price per unit in cents as an integer.
+	GetDeleted() bool              //Returns if the product has been deleted or not i.e true if deleted
+	GetBlocked() bool              //Returns if the product has been blocked or not i.e true if blocked
 }
 
 // NewProduct represents data fed into the system with the aim of creating a new product entity.
@@ -28,6 +31,7 @@ type Category interface {
 	GetDeletedAt() time.Time // Returns the deletion timestamp if the category is currently deleted or 0001-01-01 00:00:00 +0000 UTC if it is not
 	GetName() string         // Returns the name of the category
 	GetDescription() string  // Returns the category's description
+	GetBlocked() bool        //Returns if the category has been blocked or not i.e true if blocked
 }
 
 // NewCategory represents data fed into the system with the aim of creating a new Category entity.
