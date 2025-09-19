43c8d0a36a957050a9b8e59537ebf2e635b01b9b
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-09-19 10:42:43 +0300

    feat: add GetUUID() to TransactionAuthorization interface

--- transaction/transaction.go
+++ transaction/transaction.go
@@ -1,29 +1,48 @@
 package transaction
 
-import "time"
+import (
+	"time"
 
-// PurchasedProduct represents basic data fed into the system about the purchase of a particular product
+	"github.com/google/uuid"
+)
+
+// PurchasedProduct represents the essential details of a product bought as part of a transaction.
 type PurchasedProduct interface {
-	GetProductID() int64 // Returns the unique identifier of a product purchased in a transaction
-	GetUnitsBought() int // Returns the the amount of units of a product purchased in a transaction
+	GetProductID() int64 // GetProductID returns the unique identifier of a product purchased in a transaction.
+	GetUnitsBought() int // GetUnitsBought returns the number of units of a product purchased.
 }
 
-// Transaction represents a transaction entity with its properties.
+// Transaction represents the core properties of a financial transaction.
 type Transaction interface {
-	GetID() int64            // Returns the unique identifier of the transaction
-	GetUserID() int64        // Returns the unique identifier of the user that made the transaction
-	GetShopID() int64        // Returns the unique identifier of the shop that the transaction was made to
-	GetCreatedAt() time.Time // Returns the creation timestamp of the transaction
-	GetUpdatedAt() time.Time // Returns the last update timestamp of the transaction
-	GetDeletedAt() time.Time // Returns the deletion timestamp if the transaction is currently deleted or 0001-01-01 00:00:00 +0000 UTC if it is not
-	GetTotalAmountInCents() int64   // Returns the total amount spent in the transaction in cents
-	GetTransactionCostInCents() int64 // Returns the transaction cost in cents
+	GetID() int64 // GetID returns the unique identifier of the transaction.
+	GetUserID() int64 // GetUserID returns the unique identifier of the user who made the transaction.
+	GetShopID() int64 // GetShopID returns the unique identifier of the shop where the transaction occurred.
+	GetCreatedAt() time.Time // GetCreatedAt returns the creation timestamp of the transaction.
+	GetUpdatedAt() time.Time // GetUpdatedAt returns the last update timestamp of the transaction.
+	GetDeletedAt() time.Time // GetDeletedAt returns the deletion timestamp. The timestamp is a zero value if the transaction has not been deleted.
+	GetTotalAmountInCents() uint // GetTotalAmountInCents returns the total amount spent in the transaction, in cents.
+	GetTransactionCostInCents() uint // GetTransactionCostInCents returns the transaction processing cost in cents.
 }
 
-// NewTransaction represents data fed into the system with the aim of creating a new transaction entity.
-type NewTransaction interface {
-	GetPurchasedProducts() []PurchasedProduct //Returns information on all product purchases involved in the transaction
-	GetPhoneNumber() string //Returns the phone number provided by the user to authorize the transaction
-	GetPinCode() string //Returns the pin code provided by the user to authorize the transaction
+// TransactionAuthorization provides the data required for a user to authorize a transaction.
+// It is intended for embedding in more specific transaction types to avoid code duplication.
+type TransactionAuthorization interface {
+	GetUUID() uuid.UUID // GetUUID returns the unique UUID provided by the user to authorize the transaction.
+	GetPinCode() string // GetPinCode returns the security PIN code provided by the user for authorization.
 }
+
+// NewProductsTransaction represents a new transaction for a set of product purchases.
+// It embeds the TransactionAuthorization interface to include common authorization data.
+type NewProductsTransaction interface {
+	TransactionAuthorization
+	GetPurchasedProducts() []PurchasedProduct // GetPurchasedProducts returns information on all product purchases involved in the transaction.
+}
+
+// NewTransaction represents a new transaction with a single total amount, not tied to specific products.
+// It embeds the TransactionAuthorization interface to include common authorization data.
+type NewTransaction interface {
+	TransactionAuthorization
+	GetTotalAmountInCents() uint // GetTotalAmountInCents returns the total amount intended to be transacted.
+}
+
 
