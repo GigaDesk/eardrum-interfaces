9db2b9f8cb6d88208be15f5d20d13e5aacf13467
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-10-23 15:44:24 +0300

    feat: rename Shop to merchant and add nil option for GetMpesaNumber

--- user/user.go
+++ user/user.go
@@ -8,19 +8,19 @@ type User interface {
 	GetCreatedAt() time.Time // Returns the creation timestamp of the user
 	GetUpdatedAt() time.Time // Returns the last update timestamp of the user
 	GetDeletedAt() time.Time // Returns the deletion timestamp if the user is currently deleted or 0001-01-01 00:00:00 +0000 UTC if not
-	GetName() string         // Returns the name of the user
+	GetUserName() string     // Returns the username of the user
 	GetPhoneNumber() string  // Returns the phone number of the user
 	GetPassword() string     // Returns the security password of the user
 	GetAccountBalanceInCents() int64 // Returns the user's account balance in cents as an integer.
 	GetPinCode() string // Returns the user's security pin code
-	GetMpesaNumber() string  // Returns the mpesa number of the user i.e used for withdrawals
+	GetMpesaNumber() *string  // Returns the mpesa number of the user i.e used for withdrawals
 	GetQrCodeBase64() string //Returns Base64 string encoding of QR code image 
 }
 
 // NewUser represents data fed into the system with the aim of creating a new user entity.
 type NewUser interface {
-	GetName() string         // Returns the name of the user
+	GetUserName() string     // Returns the username of the user
 	GetPhoneNumber() string  // Returns the phone number of the user
 	GetPassword() string     // Returns the security password of the user
-	GetMpesaNumber() string  // Returns the mpesa number of the user i.e used for withdrawals
+	GetMpesaNumber() *string // Returns the mpesa number of the user i.e used for withdrawals
 }
