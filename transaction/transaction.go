b662cd40ab90a6f90fdf2bf03fd5edfa475b5a63
wolfofwallstreet <wolfofwallstreet@leon.leon.com>
2026-06-13 08:53:29 -0400

    refactor: remove NewProductsTransaction interface

--- transaction/transaction.go
+++ transaction/transaction.go
@@ -4,11 +4,7 @@ import (
 	"time"
 )
 
-// PurchasedProduct represents the essential details of a product bought as part of a transaction.
-type PurchasedProduct interface {
-	GetProductID() int64 // GetProductID returns the unique identifier of a product purchased in a transaction.
-	GetUnitsBought() int // GetUnitsBought returns the number of units of a product purchased.
-}
+
 
 // Transaction represents the core properties of a financial transaction.
 type Transaction interface {
@@ -27,13 +23,6 @@ type Transaction interface {
 type TransactionAuthorization interface {
 	GetUUID() string // GetUUID returns the unique UUID string provided by the user to authorize the transaction.
 	GetPinCode() string // GetPinCode returns the security PIN code provided by the user for authorization.
-}
-
-// NewProductsTransaction represents a new transaction for a set of product purchases.
-// It embeds the TransactionAuthorization interface to include common authorization data.
-type NewProductsTransaction interface {
-	TransactionAuthorization
-	GetPurchasedProducts() []PurchasedProduct // GetPurchasedProducts returns information on all product purchases involved in the transaction.
 }
 
 // NewTransaction represents a new transaction with a single total amount, not tied to specific products.
