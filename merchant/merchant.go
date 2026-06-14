1e76c25abee4970a812bc52b3b6c6a389fa5e884
wolfofwallstreet <wolfofwallstreet@leon.leon.com>
2026-06-14 06:57:49 -0400

    refactor: remove GetID method from Merchant and User interfaces

--- merchant/merchant.go
+++ merchant/merchant.go
@@ -4,11 +4,10 @@ import "time"
 
 // Merchant represents a merchant entity with its properties.
 type Merchant interface {
-	GetID() int64            // Returns the unique identifier of the merchant
 	GetCreatedAt() time.Time // Returns the creation timestamp of the merchant
 	GetUpdatedAt() time.Time // Returns the last update timestamp of the merchant
 	GetDeletedAt() time.Time // Returns the deletion timestamp if the merchant is currently deleted or 0001-01-01 00:00:00 +0000 UTC if it is not
-	GetUserName() string     // Returns the username of the merchant
+	GetUserName() string     // Returns the unique username of the merchant
 	GetPhoneNumber() string  // Returns the phone number of the merchant
 	GetPassword() string     // Returns the security password of the merchant
 	GetAccountBalanceInCents() int64 // Returns the merchant's account balance in cents as an integer.
--- user/user.go
+++ user/user.go
@@ -4,11 +4,10 @@ import "time"
 
 // User represents a user entity with its properties.
 type User interface {
-	GetID() int64              // Returns the unique identifier of the user
 	GetCreatedAt() time.Time // Returns the creation timestamp of the user
 	GetUpdatedAt() time.Time // Returns the last update timestamp of the user
 	GetDeletedAt() time.Time // Returns the deletion timestamp if the user is currently deleted or 0001-01-01 00:00:00 +0000 UTC if not
-	GetUserName() string     // Returns the username of the user
+	GetUserName() string     // Returns the unique username of the user
 	GetPhoneNumber() string  // Returns the phone number of the user
 	GetPassword() string     // Returns the security password of the user
 	GetAccountBalanceInCents() int64 // Returns the user's account balance in cents as an integer.
